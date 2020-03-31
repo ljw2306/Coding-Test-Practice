9aaa029d110a6a1fb106dc38b4c7142d7c640b07
jaewoo lee <59155484+ljw2306@users.noreply.github.com>
2020-03-31 17:50:03 +0900

    Update TestPractice9.java

--- CodingTestPractice/src/test1/TestPractice9.java
+++ CodingTestPractice/src/test1/TestPractice9.java
@@ -5,6 +5,12 @@ import java.util.List;
 
 public class TestPractice9 {
 	
+	//학생의 성적을 다시 매기는데 
+	//조건1 : 원래 점수에서 다음 5의 배수의 차가 2보다 작으면 다음 5의 배수가 점수가 된다.
+	//조건2 : 원래 점수에서 다음 5의 배수의 차가 2보다 크면 점수의 변화가 없다.
+	//조건3 : 38점 미만은 성적 올림에 해당사항이 없다.
+	//위 조건들을 만족시키는 gradingStudents 메소드를 완성하시오.
+	
 	public static List<Integer> gradingStudents(List<Integer> grades) {
 		
 		List<Integer> final_grades = new ArrayList<Integer>();
